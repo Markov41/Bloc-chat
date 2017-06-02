bc8b57cb7a5444c2bcf4f1f129d4a833040fab31
Markov41 <analogy48@yahoo.com>
2017-06-02 13:31:28 -0500

    Chat Rooms Listed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,9 @@
 (function() {
-    function HomeCtrl() {}
+    function HomeCtrl(Room, $scope) {
+        $scope.Rooms = Room.all;
+    }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ["Room", "$scope", HomeCtrl]);
 })();
